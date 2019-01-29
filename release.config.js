1f4e198d3577e95d16d719dba55a47605fe84ba9
Rhys Lloyd <rhys.lloyd@empired.com>
2019-01-29 12:08:49 +1030

    :wrench: Chore: Adds Prettier

--- release.config.js
+++ release.config.js
@@ -2,34 +2,34 @@
 module.exports = {
 	analyzeCommits: {
 		preset: 'eslint',
-		parserOpts: { // optional, only you want to have emoji commit support
+		parserOpts: {
+			// optional, only you want to have emoji commit support
 			headerPattern: /^(?::([\w-]*):)?\s*(\w*):\s*(.*)$/,
-			headerCorrespondence: [
-				'emoji',
-				'tag',
-				'message',
-			],
+			headerCorrespondence: ['emoji', 'tag', 'message'],
 		},
 	},
 	generateNotes: {
 		preset: 'eslint',
-		parserOpts: { // optional, only you want to have emoji commit support
+		parserOpts: {
+			// optional, only you want to have emoji commit support
 			headerPattern: /^(?::([\w-]*):)?\s*(\w*):\s*(.*)$/,
-			headerCorrespondence: [
-				'emoji',
-				'tag',
-				'message',
-			],
+			headerCorrespondence: ['emoji', 'tag', 'message'],
 		},
 	},
 	plugins: [
-		["@semantic-release/commit-analyzer", {
-			"preset": "eslint"
-		}],
 		[
-			"@semantic-release/release-notes-generator", {
-			"preset": "eslint"
-		}
-		]
-	]
+			'@semantic-release/commit-analyzer',
+			{
+				preset: 'eslint',
+			},
+		],
+		[
+			'@semantic-release/release-notes-generator',
+			{
+				preset: 'eslint',
+			},
+		],
+		'@semantic-release/npm',
+		'@semantic-release/github'
+	],
 };
