9bceeec62a323d7653c2fdbec6a4badf78dc3bb3
Rhys Lloyd <rhys.lloyd@empired.com>
2019-01-29 20:58:07 +1030

    :wrench: Chore: Still working on this SGC thing

--- release.config.js
+++ release.config.js
@@ -1,25 +1,34 @@
 /* eslint-disable no-useless-escape */
 module.exports = {
-	analyzeCommits: 'sgc-commit-analyzer',
-	generateNotes: {
-		preset: 'eslint',
-		parserOpts: {
-			// optional, only you want to have emoji commit support
-			headerPattern: /^(?::([\w-]*):)?\s*(\w*):\s*(.*)$/,
-			headerCorrespondence: ['emoji', 'tag', 'message'],
-		},
-	},
 	plugins: [
 		[
 			'@semantic-release/commit-analyzer',
 			{
 				preset: 'eslint',
+				parserOpts: {
+					// optional, only you want to have emoji commit support
+					headerPattern: /^(?::([\w-]*):)?\s*(\w*):\s*(.*)$/,
+					headerCorrespondence: ['emoji', 'tag', 'message'],
+				},
+				releaseRules: [
+					{ tag: 'Breaking', release: 'major' },
+					{ tag: 'Fix', release: 'patch' },
+					{ tag: 'Feat', release: 'minor' },
+					{ tag: 'New', release: 'minor' },
+					{ tag: 'Perf', release: 'patch' },
+					{ tag: 'Update',  release: 'minor' },
+				]
 			},
 		],
 		[
 			'@semantic-release/release-notes-generator',
 			{
 				preset: 'eslint',
+				parserOpts: {
+					// optional, only you want to have emoji commit support
+					headerPattern: /^(?::([\w-]*):)?\s*(\w*):\s*(.*)$/,
+					headerCorrespondence: ['emoji', 'tag', 'message'],
+				}
 			},
 		],
 		'@semantic-release/npm',
